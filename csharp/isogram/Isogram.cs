8825963ed37fa4202e082c8b69c2630c3562871d
malvinly <malvinly@outlook.com>
2020-05-31 16:54:00 -0500

    Isogram

---
+++ csharp/isogram/Isogram.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word) =>
+        word
+            .ToLower()
+            .Where(char.IsLetter)
+            .GroupBy(x => x)
+            .All(x => x.Count() == 1);
+}
