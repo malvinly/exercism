1bc79a769a7e8bf443aec46ee8ffe35407e26648
malvinly <malvinly@outlook.com>
2020-05-31 17:56:12 -0500

    Acronym

---
+++ csharp/acronym/Acronym.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        char[] chars = phrase
+            .ToUpper()
+            .Split(new [] { ' ', '-' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => x.First(char.IsLetter))
+            .ToArray();
+
+        return new string(chars);
+    }
+}
