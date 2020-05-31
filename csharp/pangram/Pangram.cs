a4e407b8f2f8c1daabf1f411e148fcafe694feae
malvinly <malvinly@outlook.com>
2020-05-31 16:33:25 -0500

    Pangram

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        string lowerCase = input.ToLower();
+
+        for (int letter = 'a'; letter <= 'z'; letter++)
+        {
+            if (!lowerCase.Contains((char)letter))
+                return false;
+        }
+
+        return true;
+    }
+}
