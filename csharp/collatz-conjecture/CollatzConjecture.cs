91cf4a7d61017c680ea681b22b16eedd0172875c
malvinly <malvinly@outlook.com>
2020-05-31 23:12:20 -0500

    CollatzConjecture

---
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    public static int Steps(int number)
+    {
+        if (number < 1)
+            throw new ArgumentOutOfRangeException(nameof(number), "Number must be greater than zero.");
+
+        int count = 0;
+        
+        while (number > 1)
+        {
+            ++count;
+
+            if (number % 2 == 0)
+                number /= 2;
+            else
+                number = number * 3 + 1;
+        }
+
+        return count;
+    }
+}
