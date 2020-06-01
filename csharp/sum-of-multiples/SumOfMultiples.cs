ce4f2504b895a743067a28dbf357ac3fcc6302e2
malvinly <malvinly@outlook.com>
2020-05-31 22:54:35 -0500

    SumOfMultiples

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        var multiplesWithoutZero = multiples.Where(x => x > 0);
+
+        return Enumerable
+            .Range(1, max - 1)
+            .Where(x => multiplesWithoutZero.Any(m => x % m == 0))
+            .Sum();
+    }
+}
